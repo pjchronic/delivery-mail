2fedda51b73f7bd087c02b59d2ff12869eca2f8a
Paulo Junior <juniorpaulo490@gmail.com>
2024-08-11 00:02:27 -0300

    front da aplicação finalizada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,165 @@
 "use client";
 
 import { useState } from "react";
-import CodeMirrorExtern from "./components/CodemirrorExtern";
 import styles from "./page.module.css";
+import { html } from "@codemirror/lang-html";
+import ReactCodeMirror from "@uiw/react-codemirror";
+import { Alert, Snackbar, SnackbarCloseReason, TextField } from "@mui/material";
 
+//#region --- Interfaces ----
 interface packageDeliveryInterface {
   template: string;
   destinatario: string;
+  assunto: string;
 }
 
+interface ErrorInterface {
+  destinyError: boolean | undefined;
+  helperDestiny: string;
+  subjectError: boolean | undefined;
+  helperSubject: string;
+}
+
+interface SnackBarInterFace {
+  openSnackBar: boolean;
+  severity?: "error" | "info" | "success" | "warning";
+  msg: string;
+}
+//#endregion
+
+const initialHtmlTemplate = `<!-- Header Table -->
+<table width="600" align="center" border="1" cellpadding="0" cellspacing="0">
+    <tr>
+        <td align="center">
+            <h1>Título</h1>
+        </td>
+    </tr>
+</table>
+
+<!-- Content Table -->
+<table width="600" align="center" border="1" cellpadding="0" cellspacing="0">
+    <tr>
+        <td>
+            <p>Conteúdo do e-mail</p>
+        </td>
+    </tr>
+</table>
+<!-- Footer Table -->
+<table width="600" align="center" border="1" cellpadding="0" cellspacing="0">
+    <tr>
+        <td align="center">
+            <p>Rodapé</p>
+        </td>
+    </tr>
+</table>`;
+
 export default function Home() {
+  const [snackBar, setSnackBar] = useState<SnackBarInterFace>({
+    openSnackBar: false,
+    severity: "info",
+    msg: "",
+  });
+
   const [packageDelivery, setPackageDelivery] =
-    useState<packageDeliveryInterface>({ template: "", destinatario: "" });
+    useState<packageDeliveryInterface>({
+      template: initialHtmlTemplate,
+      destinatario: "",
+      assunto: "",
+    });
+  const [error, setError] = useState<ErrorInterface>({
+    destinyError: undefined,
+    helperDestiny: "",
+    subjectError: undefined,
+    helperSubject: "",
+  });
 
-  console.log(packageDelivery);
+  const handleSnackBar = (sucess: boolean, msg: string) => {
+    if (sucess) {
+      setSnackBar((prev) => ({
+        ...prev,
+        openSnackBar: true,
+        severity: "success",
+        msg,
+      }));
+    } else {
+      setSnackBar((prev) => ({
+        ...prev,
+        openSnackBar: true,
+        severity: "error",
+        msg,
+      }));
+    }
+  };
 
-//   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-//     const { name, value } = event.target;
+  const handleCloseSnackBar = (
+    event?: React.SyntheticEvent | Event,
+    reason?: SnackbarCloseReason
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
 
-//     setPackageDelivery((prev) => ({
-//       ...prev,
-//       [name]: value,
-//     }));
-//   };
+    setSnackBar((prev) => ({
+      ...prev,
+      openSnackBar: false,
+    }));
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    setPackageDelivery((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+
+    const emailRegex =
+      /^([\w\.-]+@[\w\.-]+\.[a-zA-Z]{2,})(;[\w\.-]+@[\w\.-]+\.[a-zA-Z]{2,})*$/;
+
+    const testEmails = (emails: string) => emailRegex.test(emails);
+
+    switch (name) {
+      case "destinatario":
+        if (value === "") {
+          setError((prev) => ({
+            ...prev,
+            destinyError: true,
+            helperDestiny:
+              "É necessário que este campo seja preenchido com o endereço de email do destinatario",
+          }));
+        } else if (!testEmails(value)) {
+          setError((prev) => ({
+            ...prev,
+            destinyError: true,
+            helperDestiny:
+              "O campo de destinatario foi preenchido incorretamente. Você pode inserir mais de um email separando-os por ponto e vírgula",
+          }));
+        } else {
+          setError((prev) => ({
+            ...prev,
+            destinyError: false,
+            helperDestiny: "",
+          }));
+        }
+        break;
+
+      case "assunto":
+        if (value === "") {
+          setError((prev) => ({
+            ...prev,
+            subjectError: true,
+            helperSubject: "Uma informação de assunto é necessária",
+          }));
+        } else {
+          setError((prev) => ({
+            ...prev,
+            subjectError: false,
+            helperSubject: "",
+          }));
+        }
+        break;
+    }
+  };
 
   const handleChangeCodeMirror = (value: string) => {
     setPackageDelivery((prev) => ({
@@ -31,18 +168,98 @@ export default function Home() {
     }));
   };
 
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (error.destinyError === false && error.subjectError === false) {
+      //colocar try catch aqui
+      console.log(packageDelivery);
+      handleSnackBar(true, "Email enviado com sucesso");
+    } else {
+      handleSnackBar(false, "Um ou mais campos estão incorretos");
+    }
+  };
+
+  const handleReset = () => {
+    setPackageDelivery({
+      template: initialHtmlTemplate,
+      destinatario: "",
+      assunto: "",
+    });
+    setError((prev) => ({
+      ...prev,
+      subjectError: true,
+      destinyError: true,
+    }));
+  };
+
   return (
-    <main className={styles.main}>
-      <section className={styles.section}>
-        <div>
-          <h1 className="title">Insira o seu template de e-mail aqui:</h1>
-          <CodeMirrorExtern
-            name="template"
-            onChange={handleChangeCodeMirror}
-            value={packageDelivery?.template}
-          />
-        </div>
-      </section>
-    </main>
+    <>
+      <main className={styles.main}>
+        <form onSubmit={handleSubmit} className={styles.section}>
+          <div className={styles.section__module}>
+            <h1 className="title">Insira o seu template de e-mail aqui:</h1>
+            <ReactCodeMirror
+              onChange={handleChangeCodeMirror}
+              value={packageDelivery?.template}
+              height="300px"
+              width="100%"
+              theme="dark"
+              extensions={[html()]}
+            />
+          </div>
+          <div className={styles.section__module}>
+            <TextField
+              sx={{ backgroundColor: "#fff", borderRadius: "8px" }}
+              name="destinatario"
+              onChange={handleChange}
+              value={packageDelivery?.destinatario}
+              label="Destinatarios"
+              size="small"
+              multiline
+              minRows={3}
+              error={error.destinyError}
+              helperText={error.helperDestiny}
+            />
+            <TextField
+              sx={{ backgroundColor: "#fff", borderRadius: "8px" }}
+              name="assunto"
+              onChange={handleChange}
+              value={packageDelivery?.assunto}
+              label="Assunto"
+              size="small"
+              error={error.subjectError}
+              helperText={error.helperSubject}
+            />
+          </div>
+          <div className={styles.section__btn}>
+            <button
+              type="reset"
+              onClick={handleReset}
+              className={styles.btnReset}
+            >
+              Limpar
+            </button>
+            <button type="submit" className={styles.btnSubmit}>
+              Enviar
+            </button>
+          </div>
+        </form>
+      </main>
+      <Snackbar
+        open={snackBar.openSnackBar}
+        autoHideDuration={4000}
+        onClose={handleCloseSnackBar}
+      >
+        <Alert
+          onClose={handleCloseSnackBar}
+          severity={snackBar.severity}
+          variant="filled"
+          sx={{ width: "100%" }}
+        >
+          {snackBar.msg}
+        </Alert>
+      </Snackbar>
+    </>
   );
 }
